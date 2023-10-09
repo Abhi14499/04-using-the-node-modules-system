cdf4149037be1b68bdac32cb61864c0e04954ba2
Abhi14499 <abhimishra14499@gmail.com>
2023-10-10 00:18:39 +0530

    configure auto reloading of debugger

--- routes.js
+++ routes.js
@@ -26,14 +26,24 @@ const requestHandler = (req, res) => {
         res.setHeader('Location', '/');
         return res.end();
       });
+
     });
   }
+
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
   res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
+
+
+
   res.end();
+
+
+
+
 };
 
 // module.exports = requestHandler;
